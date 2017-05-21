5c3873c9d2c72812069397b19b95129d247cf93e
sangshu329 <sangshu.sun@163.com>
2017-05-22 00:19:05 +0800

    2017年5月22日00:19:03

--- MyPDO.php
+++ MyPDO.php
@@ -31,11 +31,52 @@ class MyPDO
 
     public static function getInstance($dbHost, $dbUser, $dbPassword, $dbName, $dbCharset)
     {
-        if(self::$_instance === null) {
+        if (self::$_instance === null) {
             self::$_instance = new self($dbHost, $dbUser, $dbPassword, $dbName, $dbCharset);
         }
         return self::$_instance;
     }
 
+    /* Query 查询 */
+    public function query($strSql, $queryMode = 'All', $debug = false)
+    {
+        if ($debug === true) $this->debug($strSql);
+        $recordset = $this->dbh->query($strSql);
+        $this->getPDOError();
+        if ($recordset) {
+            $recordset->setFetchMode(PDO::FETCH_ASSOC);
+            if ($queryMode == 'All') {
+                $result = $recordset->fetchAll();
+            } elseif ($queryMode == 'Row') {
+                $result = $recordset->fetch();
+            }
+        } else {
+            $result = null;
+        }
+        return $result;
+    }
+
+    /* 数据更新语句 */
+    public function update($table, $arrayDataValue, $where = '', $debug = false)
+    {
+        $this->checkFields($table, $arrayDataValue);
+        if ($where) {
+            $strSql = '';
+            foreach ($arrayDataValue as $key => $value) {
+                $strSql .= ', `' . $key . '`="' . $value . '"';
+            }
+            $strSql = substr($strSql, 0, 1);
+            $strSql = "UPDATE `$table` SET $strSql WHERE $where";
+        } else {
+            $strSql = "REPLACE INTO `$table` (`".implode('`,`', array_keys($arrayDataValue))."`) VALUES (`".impload("`,`",$arrayDataValue)."`)";
+        }
+        if ($debug === true) $this->debug($strSql);
+        $result = $this->dbh->exec($strSql);
+        $this->getPDOError();
+        return $result;
+    }
+
     
+
+
 }
